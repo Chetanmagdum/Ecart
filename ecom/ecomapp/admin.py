40771e5ab83080009a67d2b3e73036be344f7d06
chetanmagdum <chetanmagdum18@email.com>
2024-03-11 14:16:45 +0530

    first commit

---
+++ ecom/ecomapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from ecomapp.models import Product
+
+# Register your models here.
+
+# admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+ 
+ 
+ fields = ['id','name','price','ptdetails','is_active','image']
+admin.site.register(Product)
